f1221d89ff46d100431d3462ea0900c193aa8b5d
astict <thomas.astic31@gmail.com>
2026-01-26 09:16:23 +0100

    Commit du 26 janv. | 09:16

---
+++ N°10-Méthodes/App.java
@@ -0,0 +1,16 @@
+public class App
+{
+    public static void main(String[] args)
+    {
+        prog();
+
+        prog();
+    }
+
+    public static void prog()
+    {
+        System.out.println("Bonjour !");
+        System.out.println("Comment ca va ?");
+        System.out.println("Moi bien")
+    }
+}
