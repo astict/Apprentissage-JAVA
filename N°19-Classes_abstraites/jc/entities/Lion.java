52ecb7883427615864e4e994999d9d99c5bd78b3
astict <thomas.astic31@gmail.com>
2026-03-04 18:13:41 +0100

    Commit du 4 mars | 18:13

---
+++ N°19-Classes_abstraites/jc/entities/Lion.java
@@ -0,0 +1,19 @@
+package jc.entities;
+
+public class Tiger extends LivingSpecies
+{
+    public Tiger()
+    {
+        mName = "Tigre";
+    }
+
+    public void eat()
+    {
+        System.out.println("Je mange de la viande...");
+    }
+
+    public void speak()
+    {
+        System.out.println("Je rugis...");
+    }
+}
